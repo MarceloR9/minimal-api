389680c6d10b2876922512cb40a6efc81f81d3ee
Marcelo Rodrigues <marcelo.mdsr09@gmail.com>
2024-08-16 16:39:54 -0300

    regras para a autorização

---
+++ Migrations/20240802030511_SeedAdministrador.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minimal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdministrador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Administradores",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.InsertData(
+                table: "Administradores",
+                columns: new[] { "Id", "Email", "Perfil", "Senha" },
+                values: new object[] { 1, "admin@teste.com", "Admin", "123456" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Administradores",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Administradores",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
